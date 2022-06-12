4f47dbc8ee42567d5e0a236e5b917adaee04dcba
vismarlora <vismar_lora@ucne.edu.do>
2022-06-12 09:45:04 -0400

    Arreglando la validacion de los campos del Registro

--- app/src/main/java/com/example/p1_ap2_vismar_20190425/ui/theme/prestamo/Registro.kt
+++ app/src/main/java/com/example/p1_ap2_vismar_20190425/ui/theme/prestamo/Registro.kt
@@ -1,5 +1,6 @@
 package com.example.p1_ap2_vismar_20190425.ui.theme.prestamo
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,9 +12,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.navigation.NavHostController
 
 @Composable
@@ -24,6 +29,11 @@ fun RegistroPrestamo(
 
     val scaffoldState = rememberScaffoldState()
 
+    var validar = LocalContext.current
+    val focusRequester = FocusRequester()
+    val focusRequesterConcepto = FocusRequester()
+    val focusRequesterMonto = FocusRequester()
+
 
     Scaffold(
         topBar = {
@@ -31,31 +41,62 @@ fun RegistroPrestamo(
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { navHostController.navigate("NavegarConsulta")
-                  viewModel.Guardar()  
+                onClick = {
+                    if (viewModel.deudor.isNullOrEmpty()) {
+
+                        Toast.makeText(validar, "El campo Deudor está vacio!", Toast.LENGTH_LONG).show()
+                        focusRequester.requestFocus()
+                        return@FloatingActionButton
+                    }
+                    if (viewModel.concepto.isNullOrEmpty()) {
+                        Toast.makeText(validar, "El campo Concepto está vacio", Toast.LENGTH_LONG).show()
+                        focusRequesterConcepto.requestFocus()
+                        return@FloatingActionButton
+                    }
+                    if (viewModel.monto <= 0) {
+                        Toast.makeText(validar, "El Monto debe ser mayor que cero!", Toast.LENGTH_LONG)
+                            .show()
+                        focusRequesterMonto.requestFocus()
+                        return@FloatingActionButton
+                    }
+
+                    viewModel.Guardar()
+                    Toast.makeText(validar, "Se ha Guardado Correctamente!", Toast.LENGTH_LONG).show()
+                    viewModel.deudor = ""
+                    viewModel.concepto = ""
+                    viewModel.monto = 0.00f
+                    focusRequester.requestFocus()
                 }
             ) {
                 Icon(imageVector = Icons.Default.Save, contentDescription = null)
             }
         },
         scaffoldState = scaffoldState
-    ){
-        Column(Modifier.padding(it).padding(16.dp)) {
+    ) {
+        Column(
+            Modifier
+                .padding(it)
+                .padding(16.dp)) {
 
             OutlinedTextField(
                 value = viewModel.deudor,
                 label = { Text(text = "Deudor") },
                 onValueChange = { viewModel.deudor = it },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
                 leadingIcon = {
                     Icon(imageVector = Icons.Default.Person, contentDescription = null)
                 }
             )
+
             OutlinedTextField(
                 value = viewModel.concepto,
                 label = { Text(text = "Concepto") },
                 onValueChange = { viewModel.concepto = it },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequesterConcepto),
                 leadingIcon = {
                     Icon(imageVector = Icons.Default.NotAccessible, contentDescription = null)
                 }
@@ -64,8 +105,10 @@ fun RegistroPrestamo(
             OutlinedTextField(
                 value = viewModel.monto.toString(),
                 label = { Text(text = "Monto") },
-                onValueChange = {viewModel.monto = it.toFloat()},
-                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { viewModel.monto = it.toFloat() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequesterMonto),
                 leadingIcon = {
                     Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = null)
                 }
